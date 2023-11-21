82ded8fad38722a3f0886e01a40f9f3112661e3b
Carina Cordes <carina.cordes@ksb-sg.ch>
2023-11-21 13:06:10 +0100

    Add paddle a

--- pong.py
+++ pong.py
@@ -1,20 +1,27 @@
 #by @Coerres
 
 import turtle
-import os
 
 wn = turtle.Screen()
-wn.title("Pong")
+wn.title("Pong by @Coerres")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-#Score
-score_a = 0
-score_b = 0
 
-#Paddle A 
+#Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#Paddle B
+
+#Ball
+
+#Main game loop
+while True:
+    wn.update()
