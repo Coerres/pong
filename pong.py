1a28ccf61e04aae52b1006b27cf55fd9396a7534
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-05-24 15:15:29 +0200

    Setup project

--- pong.py
+++ pong.py
@@ -1,2 +1,27 @@
 import pygame
 pygame.init()
+
+screen = pygame.display.set_mode([300, 300])
+pygame.display.set_caption("Pong by @Coerres")
+
+timer = pygame.time.Clock()
+framerate = 60
+black = (0,0,0)
+white = (255, 255, 255)
+
+
+
+running = True
+while running:
+    timer.tick(framerate)
+    screen.fill((black))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+    pygame.display.flip()
+
+pygame.quit()
