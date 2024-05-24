ed342e923c9d668167c76cd194c37bc586fd7d42
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-05-24 15:23:11 +0200

    Add the game pieces

--- pong.py
+++ pong.py
@@ -9,12 +9,19 @@ framerate = 60
 black = (0,0,0)
 white = (255, 255, 255)
 
-
+# game variables
+player_y = 130
+computer_y = 130
+ball_x = 145
+ball_y = 145
 
 running = True
 while running:
     timer.tick(framerate)
     screen.fill((black))
+    player = pygame.draw.rect(screen, white, [5, player_y, 10, 40])
+    computer = pygame.draw.rect(screen, white, [285, computer_y, 10, 40])
+    ball = pygame.draw.rect(screen, white, [ball_x, ball_y, 10, 10])
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
