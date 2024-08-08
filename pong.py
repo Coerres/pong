b0da24ef0657b8c7ce088340a36adcf499606f67
Carina Cordes <carina.cordes.ch@gmail.com>
2024-08-08 22:02:13 +0200

    Draw area, paddles and ball

--- pong.py
+++ pong.py
@@ -10,12 +10,27 @@ pygame.display.set_caption("Pong-Game")
 
 clock = pygame.time.Clock()
 
+ball =  pygame.Rect(0,0,30,30)
+ball.center = (screen_width / 2, screen_height / 2)
+
+cpu = pygame.Rect(0,0,20,100)
+cpu.centery = screen_height / 2
+
+player = pygame.Rect(0,0,20,100)
+player.midright = (screen_width, screen_height / 2)
+
 while True:
     #Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    #Draw game objects
+    pygame.draw.aaline(screen, 'white', (screen_width / 2,0), (screen_width / 2, screen_height))
+    pygame.draw.ellipse(screen, 'white', ball)
+    pygame.draw.rect(screen, 'white', cpu)
+    pygame.draw.rect(screen, 'white', player)
 
     #Update display
     pygame.display.update()
