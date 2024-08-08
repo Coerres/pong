e78a98935cd05b6a62f79068aac5afc6c14ce01d
Carina Cordes <carina.cordes.ch@gmail.com>
2024-08-08 21:46:11 +0200

    Add blank-screen and game loop

--- pong.py
+++ pong.py
@@ -1,1 +1,22 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+
+screen_width = 1280
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pong-Game")
+
+clock = pygame.time.Clock()
+
+while True:
+    #Check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #Update display
+    pygame.display.update()
+    clock.tick(60)
