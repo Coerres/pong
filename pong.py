90779dc87d8a6d987fe93933dc98d82a18bfa8a0
Carina Cordes <carina.cordes.ch@gmail.com>
2024-08-29 19:45:22 +0200

    Add scoring

--- pong.py
+++ pong.py
@@ -1,4 +1,21 @@
-import pygame, sys
+import pygame, sys, random
+
+def reset_ball(): 
+    global ball_speed_x, ball_speed_y
+    ball.x = screen_width / 2 -10
+    ball.y = random.randint(10, 100)
+    ball_speed_x *= random.choice([-1, 1])
+    ball_speed_y *= random.choice([-1, 1])
+    
+def point_won(winner):
+    global cpu_points, player_points
+
+    if winner == "cpu":
+        cpu_points += 1
+    if winner == "player":
+        player_points += 1
+
+    reset_ball()
 
 def animate_ball():
     global ball_speed_x, ball_speed_y
@@ -8,7 +25,13 @@ def animate_ball():
     if ball.bottom >= screen_height or ball.top <= 0:
         ball_speed_y *= -1
 
-    if ball.right >= screen_width or ball.left <= 0:
+    if ball.right >= screen_width:
+        point_won("cpu")
+    
+    if ball.left <= 0:
+        point_won("player")
+
+    if ball.colliderect(player) or ball.colliderect(cpu):
         ball_speed_x *= -1
 
 def animate_player():
@@ -58,6 +81,10 @@ ball_speed_y = 6
 player_speed = 0
 cpu_speed= 6
 
+cpu_points, player_points = 0, 0
+
+score_font = pygame.font.Font(None, 100)
+
 while True:
     #Check for events
     for event in pygame.event.get():
@@ -84,6 +111,12 @@ while True:
 
     #Draw game objects
     screen.fill('black')
+
+    cpu_score_surface = score_font.render(str(cpu_points), True, "white")
+    player_score_surface = score_font.render(str(player_points), True, "white")
+    screen.blit(cpu_score_surface, (screen_width/4, 20))
+    screen.blit(player_score_surface, (3* screen_width/4, 20))
+
     pygame.draw.aaline(screen, 'white', (screen_width / 2,0), (screen_width / 2, screen_height))
     pygame.draw.ellipse(screen, 'white', ball)
     pygame.draw.rect(screen, 'white', cpu)
